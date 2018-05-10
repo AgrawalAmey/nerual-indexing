1218f97dcc3f0ed4d2e7a0f5c24ada8840529a18
AgrawalAmey <agrawalamey12@gmail.com>
2018-05-10 20:57:26 +0530

    made knn sensible

--- geo-transformations/deep_iri_net_triplet/train_classifier_knn.py
+++ geo-transformations/deep_iri_net_triplet/train_classifier_knn.py
@@ -2,37 +2,37 @@ import datetime
 import pickle
 
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import train_test_split
 
 # Load embeddings
 print('Loading embeddings...')
 
 train_file_name = "../../embeddings/train.p"
-val_file_name = "../../embeddings/val.p"
 
-X_train, y_train = pickle.load(open(train_file_name, 'rb'))
-X_val, y_val = pickle.load(open(val_file_name, 'rb'))
+X, y = pickle.load(open(train_file_name, 'rb'))
 
-classifier = KNeighborsClassifier(n_neighbors=2)
+classifier = KNeighborsClassifier(n_neighbors=5)
+
+print('Splitting data...')
+X_train, X_test, y_train, y_test = train_test_split(
+                X, y, test_size=0.1, random_state=42,
+                shuffle=True)
 
 # Fit the model
 print('Fitting the model...')
 classifier.fit(X_train, y_train)
 
 # Score
-print('Evaluating on train set...')
-train_score = classifier.score(X_train, y_train)
+print('Evaluating...')
+test_score = classifier.score(X_test, y_test)
 
-print('Evaluating on val set...')
-val_score = classifier.score(X_val, y_val)
-
-print("Train score: {}, Val score: {}.".format(
-                        train_score, val_score))
+print("Test score: {}.".format(
+                        test_score))
 
 print("Saving classifier model...")
 
 timestamp = datetime.datetime.now().strftime("%Y_%m_%d_%H_%M_%S")
-file_name = "../../checkpoints/deep_iris_net_triplet_knn/{}_{}_{}.p".format(timestamp,
-                                                                            train_score,
-                                                                            val_score)
+file_name = "../../checkpoints/deep_iris_net_triplet_knn/{}_{}.p".format(timestamp,
+                                                                            test_score)
 with open(file_name, 'wb') as f:
     pickle.dump(classifier, f)
